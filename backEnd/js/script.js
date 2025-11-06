baeae14f010038fbc681cd4db5a4421a121fd3c4
joaodias23 <204659025+joaodias23@users.noreply.github.com>
2025-11-06 17:53:35 +0000

    add javascript exercise

--- backEnd/js/script.js
+++ backEnd/js/script.js
@@ -1,97 +1,97 @@
-// const MESSAGE = "  TASTE THE RAINBOW!  ";
+const MESSAGE = "  TASTE THE RAINBOW!  ";
 
-// const whisper = MESSAGE.trim().toLowerCase();
+const whisper = MESSAGE.trim().toLowerCase();
 
-// console.log(whisper);
+console.log(whisper);
 
-// const word = "skateboard";
+const word = "skateboard";
 
-// const facialHair = word.slice(5).replace('o', 'e');
+const facialHair = word.slice(5).replace('o', 'e');
 
-// console.log(word);
+console.log(word);
 
-// console.log(facialHair);
+console.log(facialHair);
 
-// const students = ["Joao", 23, null, "Ola"];
+const students = ["Joao", 23, null, "Ola"];
 
-// console.log(students[1]);
+console.log(students[1]);
 
-// students[1] = 24;
+students[1] = 24;
 
-// console.log(students[1]);
+console.log(students[1]);
 
-// console.log(students.length);
+console.log(students.length);
 
-// const planetas = ["Mercúrio" , "Vénus" , "Terra" , "Mart" , "Jupiter" , "Saturno" , "Urano" , "Neptuno" , "Plutão"];
+const planetas = ["Mercúrio" , "Vénus" , "Terra" , "Mart" , "Jupiter" , "Saturno" , "Urano" , "Neptuno" , "Plutão"];
 
-// planetas[3].replace("Mart", "Marte");
+planetas[3].replace("Mart", "Marte");
 
-// planetas.pop();
+planetas.pop();
 
-// planetas.unshift("Plutinho");
+planetas.unshift("Plutinho");
 
-// console.log(planetas);
+console.log(planetas);
 
-// const airplaneSeats = [
-//     ['Ruth', 'Anthony', 'Stevie'],
-//     ['Amelia', 'Pedro', 'Maya'],
-//     ['Xavier', 'Ananya', 'Luis'],
-//     ['Luke', null, 'Deniz'],
-//     ['Rin', 'Sakura', 'Francisco']
-// ];
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
 
-// airplaneSeats[3][1] = "Joao";
+airplaneSeats[3][1] = "Joao";
 
-// console.log(airplaneSeats)
+console.log(airplaneSeats)
 
-// const product = {
-//     nome : "grandeNome",
-//     inStock : true,
-//     price : 1.99,
-//     Colors : ["vermelho" , "azul" , "verde"],
-// }
+const product = {
+    nome : "grandeNome",
+    inStock : true,
+    price : 1.99,
+    Colors : ["vermelho" , "azul" , "verde"],
+}
 
-// console.log(product.price);
-// console.log(product.Colors[2]);
+console.log(product.price);
+console.log(product.Colors[2]);
 
-// product.price = 2.55;
-// console.log(product.price + " inflação filho")
+product.price = 2.55;
+console.log(product.price + " inflação filho")
 
-// const people = ["Scooby" , "Velma" , "Daphne" , "Shaggy" , "Fred"];
+const people = ["Scooby" , "Velma" , "Daphne" , "Shaggy" , "Fred"];
 
-// for (const person of people){
-//     console.log(person.toUpperCase());
-// }
+for (const person of people){
+    console.log(person.toUpperCase());
+}
 
-// const listaCompras = [];
+const listaCompras = [];
 
-// let item;
+let item;
 
-// const codigo = "stop";
-// const apagar = "apagar";
+const codigo = "stop";
+const apagar = "apagar";
 
-// while (item != codigo) {
-//     item = prompt("Introduza os itens na lista de compras: ");
+while (item != codigo) {
+    item = prompt("Introduza os itens na lista de compras: ");
 
 
-//     let condicao = `apagar ${item.slice(6).trim()}`
+    let condicao = `apagar ${item.slice(6).trim()}`
 
-//     if (item == apagar){
-//         listaCompras.pop();
-//     } else if (item == condicao) {
-//         for (let thing of listaCompras) {
-//             if (thing == condicao.slice(6).trim()){
-//                 listaCompras.splice(thing, 1);
-//             }
-//         }
-//     } else{
-//         listaCompras.push(item);
-//     }
-// }
+    if (item == apagar){
+        listaCompras.pop();
+    } else if (item == condicao) {
+        for (let thing of listaCompras) {
+            if (thing == condicao.slice(6).trim()){
+                listaCompras.splice(listaCompras.indexOf(thing), 1);
+            }
+        }
+    } else{
+        listaCompras.push(item);
+    }
+}
 
-// listaCompras.pop();
+listaCompras.pop();
 
-// console.log(listaCompras);
+console.log(listaCompras);
 
 let message;
 
